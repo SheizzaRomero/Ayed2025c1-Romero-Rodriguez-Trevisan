f681a47069318ce9ee8d4ea054d45307fb9c4bfe
Esteban Alejo Rodriguez <estebanalejor3@gmail.com>
2025-04-26 02:07:09 -0300

    logre que funcione el run del main, no pude aun con los unittest

--- TrabajoPractico_1/proyecto_3/main.py
+++ TrabajoPractico_1/proyecto_3/main.py
@@ -1,5 +1,12 @@
 from modules.moduloguerra import JuegoGuerra
 
 if __name__ == "__main__":
-    juego = JuegoGuerra()
+    juego = JuegoGuerra(random_seed=42)
     juego.iniciar_juego(ver_partida=True)
+
+# Mostrar resultados
+    print(f"Turnos jugados: {juego.turnos_jugados}")
+    if juego.empate:
+        print("El juego terminó en empate.")
+    else:
+        print(f"El ganador es: {juego.ganador}")
