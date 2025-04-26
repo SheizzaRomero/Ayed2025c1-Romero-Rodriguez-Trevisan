71fcd3fe863d6e1a91576033f6e2236ce5123444
Esteban Alejo Rodriguez <estebanalejor3@gmail.com>
2025-04-25 23:30:31 -0300

    ya no se por que esto no anda, hice algunos cambios

--- TrabajoPractico_1/proyecto_3/main.py
+++ TrabajoPractico_1/proyecto_3/main.py
@@ -1,9 +1,5 @@
-# main.py
-from modules.juegoGuerra import JuegoGuerra
+from modules.moduloguerra import JuegoGuerra
 
 if __name__ == "__main__":
-    juego = JuegoGuerra(random_seed=314)
-    juego.iniciar_juego(ver_partida=False)
-    print(f"Turnos jugados: {juego.turnos_jugados}")
-    print(f"Ganador: {juego.ganador}")
-    print(f"Empate: {juego.empate}")
+    juego = JuegoGuerra()
+    juego.iniciar_juego(ver_partida=True)
