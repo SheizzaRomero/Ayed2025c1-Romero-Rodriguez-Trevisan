ccba7e171a78147b80e824cb3d15449583b45fad
gersioluis <elgersio.t@gmail.com>
2025-05-01 19:43:28 -0300

    Generador de los datos para los grafiquitos del segundo problema

--- TrabajoPractico_1/proyecto_2/main.py
+++ TrabajoPractico_1/proyecto_2/main.py
@@ -1,30 +1,31 @@
 # Aplicación principal
-import time
-import matplotlib.pyplot as plt
-from modules import ListaDobleEnlazada 
+import time, random, csv
+from modules.lde import ListaDobleEnlazada 
 
 def medir_tiempos(funcion, N_valores):
     tiempos = []
     for N in N_valores:
         lista = ListaDobleEnlazada()
-        for i in range(N):
-            lista.agregar_al_final(i)
+        for _ in range(N):
+            lista.agregar_al_final(random.randrange(500))
         inicio = time.time()
         funcion(lista)
         fin = time.time()
         tiempos.append(fin - inicio)
     return tiempos
-N = list(range(1000, 10001, 1000))
+
+print('* INICIO')
+
+N = list(range(10000, 100001, 10000))
 tiempos_len = medir_tiempos(lambda l: len(l), N)
 tiempos_copiar = medir_tiempos(lambda l: l.copiar(), N)
 tiempos_invertir = medir_tiempos(lambda l: l.invertir(), N)
 
-plt.plot(N, tiempos_len, label="len")
-plt.plot(N, tiempos_copiar, label="copiar")
-plt.plot(N, tiempos_invertir, label="invertir")
-plt.set_xlabel("Cantidad de elementos (N)")
-plt.set_ylabel("Tiempo (segundos)")
-plt.legend()
-plt.title("Tiempo de ejecución vs N")
-plt.grid(True)
-plt.show()
+with open('datos.csv', 'w', newline='') as archivo:
+    salida = csv.writer(archivo, dialect='excel')
+    salida.writerow(N)
+    salida.writerow(tiempos_len)
+    salida.writerow(tiempos_copiar)
+    salida.writerow(tiempos_invertir)
+
+print('* FIN')
