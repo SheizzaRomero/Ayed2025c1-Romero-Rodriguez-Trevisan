db8ef2ee92a99c36427d67b6871652edd70c4b92
gersioluis <126624340+gersioluis@users.noreply.github.com>
2025-06-13 11:29:13 -0300

    Asuntos del problema 2

--- TrabajoPractico_2/proyecto_2/main.py
+++ TrabajoPractico_2/proyecto_2/main.py
@@ -2,25 +2,30 @@ from modules.AVL import NodoArbol, ArbolBinarioBusqueda_AVL
 from datetime import date
 from modules.Temperaturas_DB import Temperaturas_DB
 
-def main():
-    db = Temperaturas_DB()
+db = Temperaturas_DB()
 
-    db.guardar_temperatura(30.1, date (2023,5,1))
-    db.guardar_temperatura(25.4, date (2023,6,1))
-    db.guardar_temperatura(28.7, date (2023,7,1))
-    db.guardar_temperatura(22.5, date (2023,8,1))
-    db.guardar_temperatura(18.3, date (2023,9,1))
-    db.guardar_temperatura(19.2, date (2023,10,1))
+db.guardar_temperatura(30.1, date (2023,5,1))
+db.guardar_temperatura(25.4, date (2023,6,1))
+db.guardar_temperatura(28.7, date (2023,7,1))
+db.guardar_temperatura(22.5, date (2023,8,1))
+db.guardar_temperatura(18.3, date (2023,9,1))
+db.guardar_temperatura(19.2, date (2023,10,1))
+print("Temperaturas guardadas:")
 
-    print("Temperaturas guardadas:")
-    print ("Maximo del rango" , db.max_temp_rango(date (2023,7,1), date (2023,9,1) ))
+print("Devolver temperatura del 1/6/2023")
+print(db.devolver_temperatura(date(2023,6,1))
+
+print ("Maximo del rango" , db.max_temp_rango(date (2023,7,1), date (2023,9,1) ))
+
+print ("Mínimo del rango" , db.min_temp_rango(date (2023,7,1), date (2023,9,1) ))
+
+print ("Temperaturas extremas del rango" , db.temp_extremos_rango(date (2023,7,1), date (2023,9,1) ))
     
-    print("Listado de temperaturas:")
-    for linea in db.devolver_temperaturas(date(2023,6,1), date(2023,9,1)):
-        print(linea)
+print("Borrar la temperatura del 1/6/2023")
+db.borrar_temperatura(date(2023,1,6))
 
-    print("Cantidad de muestras:", db.cantidad_muestras())
+print("Listado de temperaturas:")
+for linea in db.devolver_temperaturas(date(2023,6,1), date(2023,9,1)):
+    print(linea)
 
-if __name__ == "__main__":
-   main ()
-
+print("Cantidad de muestras:", db.cantidad_muestras())
